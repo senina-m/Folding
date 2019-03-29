d7a267765724f8ab220ef1969a341c7aba5370aa
Maria Senina <senina.m.m@yandex.ru>
2019-03-29 23:35:05 +0300

    SDR algorithm no bugs now

--- SDR.py
+++ SDR.py
@@ -1,13 +1,16 @@
 import scipy.optimize as sc
 import numpy as np
 import math
+
 '''
 Жалкая попытка повторить алгоритм SDR
 попытка в разработке
 '''
 
+
 def fun(D, W, lambd):
     n = len(D[0])
+    list = np.zeros(int(n * (n + 1) / 2))
     x = -1 / (n + math.sqrt(n))
     y = -1 / math.sqrt(n)
     V = np.concatenate(y * np.ones(1, n - 1), x * np.ones(n - 1) + np.eye(n - 1))
@@ -19,18 +22,19 @@ def fun(D, W, lambd):
 
     def ineq_constraint(x):
         return x
+
     constraints = {'type': 'ineq', 'fun': ineq_constraint}
 
     def f(list):
         k = 0
-        for i in range n:
-            for j in range n:
+        for i in range(n):
+            for j in range(n):
                 G[i][j] = list[k]
                 G[j][i] = list[k]
             k = k + 1
         return np.trace(G) - lambd * np.linalg.norm(np.multiply(W, (E - D)))
 
-    M = sc.minimize(f(), list, method='nelder-mead', constraints=constraints, options={'xtol': 1e-8, 'disp': True})
+    M = sc.minimize(f, list, method='nelder-mead', constraints=constraints, options={'xtol': 1e-8, 'disp': True})
     U, S, V = np.linalg.svd(B)
     EDM = np.diag(B) * e.T() + e * np.diag(B).T() - 2 * B
     X = math.sqrt(S) * V.T()
