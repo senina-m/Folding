afa11254691332fa38e766876a6f5778cc8b555d
Maria Senina <senina.m.m@yandex.ru>
2019-09-23 22:31:22 +0300

    Convolution neural network about proteins - all unusefull is deleted based on keras

--- test5.py
+++ test5.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 from Bio import SeqIO
 from Bio.PDB import *
 import numpy as np
@@ -41,8 +44,6 @@ def get_edm(filename):
 filename = "6mep.fasta.txt"
 structure_name = "6mep"
 
-
-
 list_of_fasta_arrs = []
 for seq_record in SeqIO.parse(filename, "fasta"):
     list_of_fasta_arrs.append(list(seq_record.seq[:]))
@@ -60,75 +61,10 @@ with tf.compat.v1.Session() as sess:
     onehot_encoded_sequence = sess.run(onehot, feed_dict={t: list_of_fasta_arrs[0]})
 ONE_HOT_ENCODING_LEN = len(onehot_encoded_sequence[0])
 
-
-def cnn_model_fn(features, labels, mode):
-    """Model function for CNN."""
-    # Input Layer
-    input_layer = tf.reshape(features["x"], [-1, features.shape[0], ONE_HOT_ENCODING_LEN, 1])
-
-    # Convolutional Layer #1
-    conv1 = tf.layers.conv2d(
-        inputs=input_layer,
-        filters=32,
-        kernel_size=[5, 5],
-        padding="same",
-        activation=tf.nn.relu)
-
-    # Pooling Layer #1
-    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-
-    # Convolutional Layer #2 and Pooling Layer #2
-    conv2 = tf.layers.conv2d(
-        inputs=pool1,
-        filters=64,
-        kernel_size=[5, 5],
-        padding="same",
-        activation=tf.nn.relu)
-
-    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-
-    # # Dense Layer
-    # pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-    # dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-    # dropout = tf.layers.dropout(
-    #     inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
-
-    # Logits Layer
-    logits = tf.layers.dense(inputs=pool2, units=features.shape[0] * 5)
-    print(logits.shape)
-
-    # making a right result to compare with it
-    example = get_edm(structure_name)
-    mse = tf.reduce_mean(tf.squared_difference(logits, example.reshape(1,)))
-
-    # TODO: loss function and training
-
-    # predictions = {
-    #     # Generate predictions (for PREDICT and EVAL mode)
-    #     "classes": tf.argmax(input=logits, axis=1),
-    #     # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
-    #     # `logging_hook`.
-    #     "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-    # }
-    #
-    # if mode == tf.estimator.ModeKeys.PREDICT:
-    #     return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
-    #
-    # # Calculate Loss (for both TRAIN and EVAL modes)
-    # loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-    #
-    # # Configure the Training Op (for TRAIN mode)
-    # if mode == tf.estimator.ModeKeys.TRAIN:
-    #     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-    #     train_op = optimizer.minimize(
-    #         loss=loss,
-    #         global_step=tf.train.get_global_step())
-    #     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-    #
-    # # Add evaluation metrics (for EVAL mode)
-    # eval_metric_ops = {
-    #     "accuracy": tf.metrics.accuracy(
-    #         labels=labels, predictions=predictions["classes"])
-    # }
-    # return tf.estimator.EstimatorSpec(
-    #     mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+model = tf.keras.Sequential()
+# add model layers
+model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='valid', data_format=None))
+model.add(Flatten())
